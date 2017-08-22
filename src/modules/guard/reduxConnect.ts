db4897396a21265b6b0097f4570cc7f3d12e22e7
goldenbearkin <huihangkin@gmail.com>
2017-08-22 19:49:34 +0800

    clean up some unnecessary inline comments

--- src/modules/guard/reduxConnect.ts
+++ src/modules/guard/reduxConnect.ts
@@ -1,12 +1,8 @@
 import { combineReducers } from 'redux';
-// import { fork, all } from 'redux-saga/effects';
 import { authReducer, AuthStateT } from './private/redux/auth.reducers';
 import { loginReducer, LoginStateT } from './private/redux/login.reducers';
 import { logoutReducer, LogoutStateT } from './private/redux/logout.reducers';
 export { guardFlowSaga } from './private/redux/guard.sagas';
-
-// import { watchLogin } from './private/redux/login.sagas';
-// import { watchLogout } from './private/redux/logout.sagas';
 
 export type GuardStateT = {
   auth: AuthStateT,
@@ -20,5 +16,4 @@ export const guardReducer = combineReducers({
   logout: logoutReducer
 });
 
-// export { AuthStateT, LoginStateT, LogoutStateT, GuardStateT };
 export { RootStateT } from '../../store';
