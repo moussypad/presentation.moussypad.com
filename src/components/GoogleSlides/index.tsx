1eebcc7ccf6e6db6cbe210135164ffca6c096ddb
goldenbearkin <huihangkin@gmail.com>
2017-08-28 02:07:30 +0800

    add GoogleSlides

---
+++ src/components/GoogleSlides/index.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+
+type PropsT = {
+  id: string;
+  style?: React.CSSProperties;
+};
+
+class GoogleSlide extends React.PureComponent<PropsT> {
+  render() {
+    const { id, style } = this.props;
+    return (
+      <div style={{ ...style, position: 'relative' }}>
+        <iframe
+          style={{ width: '100%', height: '100%' }}
+          src={`https://docs.google.com/presentation/d/${id}/embed`}
+          frameBorder={0}
+        />
+        <img
+          style={{ position: 'absolute', bottom: 2, left: 272, width: 26, height: 26, backgroundColor: '#323232' }}
+          src={require('./private/assets/exit.png')}
+          alt="exit"
+        />
+      </div>
+    );
+  }
+}
+
+export default GoogleSlide;
---
+++ src/components/GoogleSlides/private/__stories__/GoogleSlides.stories.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+import GoogleSlides from '../../../GoogleSlides';
+
+storiesOf('GoogleSlides', module)
+  .add('<GoogleSlides />', () => <GoogleSlides id="1AmsefLlotWWvp_uC-p2urXpDvu8Vh5fxQPv4-zGuBOM" style={{ width: 500, height: 500 }} />);
